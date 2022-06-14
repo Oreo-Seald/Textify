555c372bd42d6b9c9e3098ce80be9f15343a83c0
Oreo-Seald <harshal4963@gmail.com>
2022-06-14 11:16:15 +0530

    Updates

---
+++ precache-manifest.d012d4f7237709c5e59523c4dd333dcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c82d0f6582874dbd81f7",
+    "url": "/Textify/static/css/main.bfb0c685.chunk.css"
+  },
+  {
+    "revision": "c82d0f6582874dbd81f7",
+    "url": "/Textify/static/js/main.c82d0f65.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/Textify/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "5b977f20bbc5700a25d3",
+    "url": "/Textify/static/js/2.5b977f20.chunk.js"
+  },
+  {
+    "revision": "504755d7ecb7fe769354",
+    "url": "/Textify/static/js/runtime~main.504755d7.js"
+  },
+  {
+    "revision": "113727ecb4f7d7f8a9e77b37d9d73b0a",
+    "url": "/Textify/index.html"
+  }
+];
